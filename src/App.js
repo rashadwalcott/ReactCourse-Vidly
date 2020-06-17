4a76dd6269146a022dd39d103e9be06751b7aa10
Rashad <rashad.walcott15@gmail.com>
2020-06-17 16:12:42 -0400

    Register Forms added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Movies from "./components/movies";
 import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./components/movies";
 import Rentals from "./components/rentals";
 import Customers from "./components/customers";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/register" component={RegisterForm} />
           <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
