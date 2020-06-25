cdacd59645287b605d24c96c858e7727c9355fcd
Rashad <rashad.walcott15@gmail.com>
2020-06-25 17:20:33 -0400

    Populating the form

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -2,9 +2,16 @@ import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = `${apiUrl}/movies`;
+
 export function getMovies() {
   return http.get(apiEndpoint);
 }
+
+export function getMovie(movieId) {
+  return http.get(`${apiEndpoint}/${movieId}`);
+}
+
+export function saveMovie(movie) {}
 
 export function deleteMovie(movieId) {
   return http.delete(`${apiEndpoint}/${movieId}`);
